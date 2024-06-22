fab4858ab6965009a76df486554dfd90be265a27
Ismo Vuorinen <ismo@ivuorinen.net>
2024-06-23 01:21:33 +0300

    chore: configure laytan/cloak.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -111,7 +111,37 @@ return {
   },
   -- Cloak allows you to overlay *'s over defined patterns in defined files.
   -- https://github.com/laytan/cloak.nvim
-  { 'laytan/cloak.nvim', enabled = true, lazy = false },
+  {
+    'laytan/cloak.nvim',
+    enabled = true,
+    lazy = false,
+    config = function()
+      require('cloak').setup {
+        enabled = true,
+        cloak_character = '*',
+        -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+        highlight_group = 'Comment',
+        patterns = {
+          {
+            -- Match any file starting with ".env".
+            -- This can be a table to match multiple file patterns.
+            file_pattern = {
+              '.env*',
+              'wrangler.toml',
+              '.dev.vars',
+            },
+            -- Match an equals sign and any character after it.
+            -- This can also be a table of patterns to cloak,
+            -- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
+            cloak_pattern = '=.+',
+          },
+        },
+      }
+    end,
+    keys = {
+      { '<leader>tc', '<cmd>CloakToggle<cr>', desc = 'Toggle Cloak' },
+    },
+  },
   -- Not UFO in the sky, but an ultra fold in Neovim.
   -- https://github.com/kevinhwang91/nvim-ufo/
   {
